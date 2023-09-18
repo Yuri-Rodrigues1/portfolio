e3bc66e176bb142a4b482f6afae777fa7b1b1673
teste <wyurirodrigues@gmail.com>
2023-09-18 14:28:21 -0300

    adcionando o primeiro projeto no portfolio

--- src/pages/projetos/Cards.js
+++ src/pages/projetos/Cards.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import kanban from "./img/kanban.PNG"
 
 export default function Cards() {
     const cards = [{
-        titulo: 'Projeto 1',
-        imagem: 'https://imgs.search.brave.com/ZNjqoRbyMh-laqmbhbQWoWrMv5XsRQBJOIFDnVizWXI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTE4/MTQ4NDg3My9waG90/by9nb29nbGUuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPTYw/SEJIT1ppWG1Ga052/QzRUb19xRl9MWnJ1/NHFhT3F0NlFKLUtE/MUFBTWM9',
+        titulo: 'Kanban',
+        imagem: kanban,
+        src: "https://z6scrf.csb.app/"
     },
     {
         titulo: 'Projeto 2',
-        imagem: 'https://imgs.search.brave.com/ZNjqoRbyMh-laqmbhbQWoWrMv5XsRQBJOIFDnVizWXI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTE4/MTQ4NDg3My9waG90/by9nb29nbGUuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPTYw/SEJIT1ppWG1Ga052/QzRUb19xRl9MWnJ1/NHFhT3F0NlFKLUtE/MUFBTWM9',
+        imagem: kanban,
+        src: "https://z6scrf.csb.app/"
     },
     {
       titulo: 'Projeto 1',
       imagem: 'https://imgs.search.brave.com/ZNjqoRbyMh-laqmbhbQWoWrMv5XsRQBJOIFDnVizWXI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTE4/MTQ4NDg3My9waG90/by9nb29nbGUuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPTYw/SEJIT1ppWG1Ga052/QzRUb19xRl9MWnJ1/NHFhT3F0NlFKLUtE/MUFBTWM9',
+      src: "https://z6scrf.csb.app/"
   },
   {
       titulo: 'Projeto 2',
       imagem: 'https://imgs.search.brave.com/ZNjqoRbyMh-laqmbhbQWoWrMv5XsRQBJOIFDnVizWXI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTE4/MTQ4NDg3My9waG90/by9nb29nbGUuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPTYw/SEJIT1ppWG1Ga052/QzRUb19xRl9MWnJ1/NHFhT3F0NlFKLUtE/MUFBTWM9',
+      src: "https://z6scrf.csb.app/"
   },]
   return (
     <div>
@@ -25,7 +30,7 @@ export default function Cards() {
           {cards.map((card)=>(
             <div className='card'>
 
-             <a href='https://www.youtube.com/'>{card.titulo}</a>
+             <a href={card.src}>{card.titulo}</a>
              <img alt="imagem do projeto"className='projeto-img'src={card.imagem}></img> 
             </div>
           ))}
