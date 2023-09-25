303fc30c36bebf6aeaeef67667f85ab77cb60b39
teste <wyurirodrigues@gmail.com>
2023-09-25 13:46:02 -0300

    comitando o projeto Rick and Morty

--- src/pages/projetos/Cards.js
+++ src/pages/projetos/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import kanban from "./img/kanban.PNG"
 import tradutor from "./img/tradutor.PNG"
+import RickyAndMorty from "./img/RickyAndMorty.PNG"
 
 export default function Cards() {
     const cards = [{
@@ -14,9 +15,9 @@ export default function Cards() {
         src: "https://x9mt8x.csb.app/"
     },
     {
-      titulo: 'Projeto 1',
-      imagem: 'https://imgs.search.brave.com/ZNjqoRbyMh-laqmbhbQWoWrMv5XsRQBJOIFDnVizWXI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTE4/MTQ4NDg3My9waG90/by9nb29nbGUuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPTYw/SEJIT1ppWG1Ga052/QzRUb19xRl9MWnJ1/NHFhT3F0NlFKLUtE/MUFBTWM9',
-      src: "https://z6scrf.csb.app/"
+      titulo: 'Ricky & Morty',
+      imagem: RickyAndMorty,
+      src: "https://yzdy8n.csb.app/"
   },
   {
       titulo: 'Projeto 2',
